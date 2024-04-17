b26b39ab0eac659247948c12c6466d4effee403e
krkarma777 <krkarma777@gmail.com>
2024-04-17 18:43:47 +0900

    회원가입 컨트롤러 에러 출력 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -8,6 +8,16 @@ export class UsersController {
 
     @Post('signup')
     async signUp(@Body() createUserDto: CreateUserDto): Promise<any> {
-        return await this.usersService.create(createUserDto);
+        try {
+            return await this.usersService.create(createUserDto);
+        } catch (error) {
+            // 에러 메시지를 바로 접근
+            if (error.message === '이미 등록된 이메일입니다.') {
+                throw new HttpException(error.message, HttpStatus.CONFLICT);
+            } else {
+                // 다른 모든 서버 에러에 대한 처리
+                throw new HttpException('서버 에러', HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
     }
 }
