a4c22c3f5a3237c0314725b3ca0f39af95f2a709
krkarma777 <krkarma777@gmail.com>
2024-04-17 14:53:57 +0900

    데이터베이스 연결 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'oracle',
+      host: 'localhost',
+      port: 1521,
+      username: 'krkarma777',
+      password: 'root',
+      sid: 'xe', // or serviceName: 'yourServiceName'
+      synchronize: false, // 개발 단계가 아닐 경우 false 추천
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      logging: false // 필요에 따라 true로 설정
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
