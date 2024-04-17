a7aed03c76e0ba89884f21c561e13e08259e6dcc
krkarma777 <krkarma777@gmail.com>
2024-04-17 14:53:20 +0900

    회원가입 서비스 로직 구현

---
+++ src/users/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) {}
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        const newUser = this.usersRepository.create(createUserDto);
+        await this.usersRepository.save(newUser);
+        return newUser;
+    }
+}
