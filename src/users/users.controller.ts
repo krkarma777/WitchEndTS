4e6bc93158f26f601fb8c76fbbf9c8c90114a473
krkarma777 <krkarma777@gmail.com>
2024-04-18 10:39:44 +0900

    enhance(UsersController): 불필요한 예외 처리 제거
    이 커밋은 UsersController에서 불필요한 예외 처리를 제거합니다. 불필요한 예외 처리가 제거되어 코드가 더욱 간결해졌습니다.
    
    수정 사항:
    - signUp 메서드: 에러 처리 로직 삭제
    - signIn 메서드: 에러 처리 로직 삭제
    
    관련 이슈 번호: 없음

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,21 +6,12 @@ import {UpdateUserDto} from "./dto/update-user.dto";
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {
+    }
 
     @Post('signup')
     async signUp(@Body() createUserDto: CreateUserDto): Promise<any> {
-        try {
-            return await this.usersService.create(createUserDto);
-        } catch (error) {
-            // 에러 메시지를 바로 접근
-            if (error.message === '이미 등록된 이메일입니다.') {
-                throw new HttpException(error.message, HttpStatus.CONFLICT);
-            } else {
-                // 다른 모든 서버 에러에 대한 처리
-                throw new HttpException('서버 에러', HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+        return await this.usersService.create(createUserDto);
     }
 
     @Post('login')
