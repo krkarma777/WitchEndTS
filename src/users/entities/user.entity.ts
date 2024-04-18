fe23ff26d6e16463a29ee623df5eebaa9dadedb2
krkarma777 <krkarma777@gmail.com>
2024-04-18 09:32:50 +0900

    feat(user): 사용자 엔티티 업데이트 및 추가 필드와 관계 정의
    이 커밋은 사용자 엔티티를 업데이트하여 추가적인 필드인 username, nickname, role 및 status를 포함하도록 합니다. 또한 GameCharacter 엔티티와의 일대다 관계를 설정하여 사용자가 여러 게임 캐릭터를 가질 수 있도록 합니다. 또한 엔티티 인스턴스를 초기화하기 위한 생성자와 DTO를 사용하여 사용자 정보를 업데이트하는 update 메서드가 추가되었습니다.
    
    수정 사항:
    - 사용자 엔티티에 username, nickname, role 및 status 필드 추가
    - GameCharacter 엔티티와 일대다 관계 설정
    - 필수 필드를 사용하여 엔티티 인스턴스를 초기화하는 생성자 추가
    - DTO를 사용하여 사용자 정보를 업데이트하는 update 메서드 추가

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,13 +1,47 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import {GameCharacter} from "../../game-characters/entities/game-characters.entity";
+import {UpdateUserDto} from "../dto/update-user.dto";
+import {UserStatus} from "../../enums/user-status.enum";
+import {UserRole} from "../../enums/user-role.enum";
+
 
 @Entity()
 export class User {
+
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({ unique: true })
+    @Column({ nullable: false, unique: true })
+    username: string;
+
+    @Column({ nullable: false })
+    password: string;
+
+    @Column({ nullable: false })
+    nickname: string;
+
+    @Column({ nullable: false, unique: true })
     email: string;
 
-    @Column()
-    password: string;
+    @Column({ nullable: false, default: UserRole.ROLE_USER })
+    role: UserRole;
+
+    @Column({ nullable: false, default: UserStatus.INACTIVE })
+    status: UserStatus;
+
+    @OneToMany(() => GameCharacter, gameCharacter => gameCharacter.user, { cascade: true, onDelete: 'CASCADE' })
+    gameCharacters: GameCharacter[];
+
+    constructor(username: string, password: string, nickname: string, email: string) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.email = email;
+    }
+
+    update(requestDTO: UpdateUserDto) {
+        this.nickname = requestDTO.nickname;
+        this.password = requestDTO.newPassword;
+        this.email = requestDTO.email;
+    }
 }
