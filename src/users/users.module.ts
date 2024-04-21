424945e968c3050fab6fb0b124bead3e74d0070b
krkarma777 <krkarma777@gmail.com>
2024-04-21 15:14:52 +0900

    UsersModule에 AuthModule 통합
    변경 사항: UsersModule에 AuthModule을 추가하여 인증 기능을 통합함.
    목적: UsersModule에서 AuthModule의 인증 기능을 활용하여 사용자 인증 처리를 강화하기 위함.
    영향: 이제 UsersModule에서도 인증 로직을 활용할 수 있으며, User 관련 API 요청 처리 시 인증 검증이 가능해짐.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    AuthModule
+  ],
   providers: [UsersService],
   controllers: [UsersController],
 })
